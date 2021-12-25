9aad91c9e5a08c42dcbaeec94cefeae3d5f5dfa6
becivells <732903873@qq.com>
2021-12-25 23:36:03 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+    title: 'fofax',
+    description: 'fofax 是一款简单、快速、扩展规则，联动其他工具的命令行软件',
+    base:"/",
+    themeConfig: {
+        repo: 'xiecat/fofax',
+        repoLabel: 'GitHub',
+        docsRepo: 'xiecat/fofax-doc',
+        docsDir: 'docs',
+        docsBranch: 'dev',
+        // 默认为 false，设置为 true 来启用
+        editLinks: true,
+        // 自定义编辑链接的文本。默认是 "Edit this page"
+        editLinkText: '帮助改进页面内容!',
+        nav:[ // 导航栏配置
+            { text: '主页', link: '/' },
+            { text: '开始', link: '/guide/' },
+            { text: 'Fx', link: '/fx/' },
+            { text: 'FAQ', link: '/faq/' },
+        ],
+        // 默认值是 true 。设置为 false 来禁用所有页面的 下一篇 链接
+        // nextLinks: false,
+        // // 默认值是 true 。设置为 false 来禁用所有页面的 上一篇 链接
+        // prevLinks: false,
+        lastUpdated: 'Last Updated', //
+        // sidebar: 'auto', // 侧边栏配置
+        sidebarDepth: 4, // 侧边栏显示4级
+        sidebar:{
+            '/guide/': [
+                '',
+            ],
+            '/fx/': [
+             '',
+            ],
+            '/faq/': [
+                '',
+            ],
+
+        }
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: https://socialify.git.ci/xiecat/fofax/image?description=1&font=Rokkitt&forks=1&issues=1&language=1&owner=1&pattern=Floating%20Cogs&pulls=1&stargazers=1&theme=Light
+actionText: 开始 →
+actionLink: /guide/
+features:
+- title: 扩展语法
+  details: 不仅仅支持 fofa 语法,还能通过扩展语法进行内置规则或者自定义文件规则查询
+- title: 一键浏览器打开
+  details: 无论是普通查询、ico、网站证书，在查询过程可以随时用浏览器打开 fofa 相应网页
+- title: 联动其他工具
+  details: 支持管道文件命令行参数输入，还可以对接其他工具如httpx、nuclei。  
+footer:  Copyright © 2021 xiecat
+---
